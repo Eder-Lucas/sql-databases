770552adb48b0616de0b19d6e838a052b71d52ec
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2025-04-22 22:03:16 -0300

    fiz a aula01

---
+++ sql-server/exercicios/aula01/inicio.sql
@@ -0,0 +1,15 @@
+-- ╔══════════════════════════════╗
+-- ║     APRESENTAÇÃO PESSOAL     ║
+-- ╚══════════════════════════════╝
+
+-- Nome: Éder Lucas
+-- Idade: 17 anos
+-- Cidade: Palmares - PE
+-- Comecei meus estudos em banco de dados no SSMS.
+
+-- Objetivo:
+-- Aprender mais sobre bancos de dados e entender como a informação é armazenada e manipulada.
+
+-- Sucesso e forças para mim. Tudo começa no primeiro Hello World! 🧠💾
+
+SELECT 'Hello, World!' AS Mensagem;
