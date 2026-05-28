898346bc72ad7d85ca9c9d04ad83445036a3acec
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-05-28 15:32:29 -0300

    feat: adiciona stored procedure de consulta de produtos

--- sqlServer/aulas/aula13/aula13_ex001_storedProcedure.sql
+++ sqlServer/aulas/aula13/aula13_ex001_storedProcedure.sql
@@ -1,7 +1,30 @@
+USE Aula13_ex001
+GO
+
 -- Cria uma procedure para inserir uma categoria
 CREATE PROCEDURE insereCategoria
-@DescricaoCategoria NVARCHAR(50)
+	@DescricaoCategoria NVARCHAR(50)
 AS
+BEGIN
 	INSERT INTO Categorias(DESCRICAO_CATEGORIA)
 	VALUES (@DescricaoCategoria)
+END
+GO
+
+-- Essa procedure exibe os registros da tabela Produtos
+CREATE PROCEDURE mostraProdutos
+	@idProduto INT = NULL -- Variável opcional, sem ela teria que fazer: EXEC mostraProdutos NULL
+AS
+BEGIN
+	IF @idProduto IS NOT NULL
+	BEGIN
+		SELECT * FROM Produtos
+		WHERE ID_PRODUTO = @idProduto
+	END
+
+	ELSE
+	BEGIN
+		SELECT * FROM Produtos
+	END
+END
 GO
